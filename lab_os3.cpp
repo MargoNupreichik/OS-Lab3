470f51afcf2a131dfd22ae5500fb4f3dcad6fc0a
arlenochka <m.nupreichik@yandex.ru>
2024-02-29 19:15:44 +0300

    version 2

--- lab_os3.cpp
+++ lab_os3.cpp
@@ -15,7 +15,7 @@ typedef struct {
 }targs;
 
 int fd[2];
-int size_buff = 1024;
+int size_buff = 512;
 
 void* process_writer(void *arg){
 	printf("\nthread1 started working\n");
@@ -23,16 +23,21 @@ void* process_writer(void *arg){
 	
 	// create buffer
 	char buff[size_buff];
+	
 	rlimit rlim;
-	int counter = 0;
+	
 	while(args->flag == 0) {
-		getrlimit(RLIMIT_CPU, &rlim);
-		strcpy(buff, (std::__cxx11::to_string(rlim.rlim_max)).c_str());
-		write(fd[1], &buff, counter);
-		counter++;
-		if (counter >= size_buff) counter = 0;
+		memset(buff, 0, size_buff);
+		getrlimit(RLIMIT_AS, &rlim);
+		size_t counter = (size_t)(sprintf(buff, "%ju", (uintmax_t)rlim.rlim_cur));
+		// sprintf(buff, "%li", rlim.rlim_max);
+		int rv = write(fd[1], buff, counter);
+		if (rv == -1){
+			printf("Writer: error %s\n", strerror(rv));
+		} else {
+			printf("Writer: %s\n", buff);
+		}
 		sleep(1);
-		strcpy(buff, "\0");	
 	}
 	
 	pthread_exit((void*)1);
@@ -42,39 +47,52 @@ void* process_reader(void *arg){
 	printf("\nthread2 started working\n");
 	targs *args = (targs*) arg;
 	
-	char* buffer;
+	char buffer[size_buff];
 	errno = 0;
 	
-	int res_read;
+	int rv;
 	
 	while(args->flag == 0) {
-		res_read = read(fd[0], &buffer, size_buff);
-		if (res_read == 0) {
+		memset(buffer, 0, size_buff);
+		rv = read(fd[0], buffer, (size_t)size_buff);
+		if (rv == 0) {
 			printf("Reader: No data or end of file\n");
 		}
-		if (res_read == -1) {
+		if (rv == -1) {
 			printf("Reader: error - %s\n", strerror(errno));
 		}
-		if (res_read > 0){
-			printf("Reader: %c\n", buffer);
+		if (rv > 0){
+			printf("Reader: %s\n", buffer);
 		}
 		sleep(1);	
 	}	
-	
 	pthread_exit((void*)2);
 }
 
-int main() {
+int main(int argc, char* argv[]) {
 	printf("programm started working\n");
 	
 	targs arg1; targs arg2;
 	arg1.flag = 0; arg2.flag = 0;
 	arg1.symb = '1'; arg2.symb = '2';
 	
+	int rv;
+	
+	if (strcmp(argv[1], "1\0") == 0){
+		rv = pipe(fd);
+	}
+	else if (strcmp(argv[1], "2\0") == 0){
+		pipe2(fd, O_NONBLOCK);
+	}
+	else if (strcmp(argv[1], "3\0") == 0){
+		rv = pipe(fd);
+		fcntl(fd[0], F_SETFL, O_NONBLOCK);
+		fcntl(fd[1], F_SETFL, O_NONBLOCK);
+	}
+	else return 0;
+	
 	pthread_t id1;
 	pthread_t id2;
-	
-	pipe(fd);
 	
  	pthread_create(&id1, NULL, process_writer, &arg1);
  	pthread_create(&id2, NULL, process_reader, &arg2);
